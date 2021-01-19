d061934ce29eb1a153bd6134d07b43ed5e2d5858
Łukasz Podolak <lukasz.podolak@gmail.com>
2021-01-19 17:49:41 +0100

    VB-2053 sparkles: use ezyBuild-Rake with dockerized nuget via mono image
    * Can't use nuget directly, as it will not work on teamcity. TC, doesn't support Nuget steps neither on our agents
    * So I had to use dockerized packaging. I Tried using dotnet restore command, but this requires .csproj, produces obj/bin folders, crashes and tons of other problems.
    * Much better way is to use nuget under mono image where we pass packages.config.
    * We have to specify the ezyBuild-Rake veresion twice: in packages.config and in rakefile.rb and I don't have easy remedy for that.
    * The order of *.rb files that needs to be referenced matters and is different under windows vs linux (TC)
    * Get ezyBuild-Rake version from packages.config to have only 1 place to bump it
    * Just did the same for VivaAir and VivaAerobus.Generic.Api projects

--- rake-build/Build.rb
+++
@@ -1,7 +0,0 @@
-include Rake::DSL
-
-class Build
-  def project(dockerfile, image_name, build_number)
-    sh "docker build -t #{image_name}:#{build_number} -f #{dockerfile} --build-arg version=#{build_number} ."
-  end
-end
