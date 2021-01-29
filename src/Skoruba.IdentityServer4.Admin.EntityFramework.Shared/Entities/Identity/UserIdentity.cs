72e99078f1b61f06a37a508d7929bc02c846fae6
Piotr Wielinski <wielinski90@gmail.com>
2021-01-29 08:58:41 +0100

    EZYC-2841 :sparkles: added new property, password history, to UserIdentity

--- src/Skoruba.IdentityServer4.Admin.EntityFramework.Shared/Entities/Identity/UserIdentity.cs
+++ src/Skoruba.IdentityServer4.Admin.EntityFramework.Shared/Entities/Identity/UserIdentity.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace Skoruba.IdentityServer4.Admin.EntityFramework.Shared.Entities.Identity
 {
 	public class UserIdentity : IdentityUser
 	{
-		
-	}
+        public List<Password> PasswordsHistory { get; set; }
+    }
+
+    public class Password
+    {
+        public string PasswordHash { get; set; }
+        public DateTime? ChangePasswordDate { get; set; }
+    }
 }
