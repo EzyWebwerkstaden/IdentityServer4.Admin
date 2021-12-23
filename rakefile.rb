e0cdc99b20fb9f91c450b76f214cdc0480e9f438
Podolak, Lukasz <lukasz.podolak.ctr@sabre.com>
2021-12-23 14:21:16 +0100

    EZYC-4500 :sparkles: upgrade ezyBuild-Rake to latest
    * let's pass the GCR_PAT via .env file so that the secret value can't be seen in console output
    * nuget.config is no longer needed
    * update ezy.dotnetcoresdk images to latest version as it contains fixed github token handling

--- rakefile.rb
+++ rakefile.rb
@@ -2,16 +2,20 @@ require 'rexml/document'
 include REXML
 root = File.dirname(__FILE__)
 
-# Section 1 - Bootstrapper: fetch ezyBuild-Rake from our nuget package in GitHub. GCR_PAT env var is required to be present.
+# Section 1 - Create .env file with all env variables that we don't want to be shown in the console.
+puts "creating .env file."
+File.open(".env", "w") do |f|
+  f.write("GCR_PAT=#{ENV["GCR_PAT"]}")
+end
+
+# Section 2 - Bootstrapper: fetch ezyBuild-Rake from our nuget package in GitHub. GCR_PAT env var is required to be present (in .env file).
 packages_xml = Document.new(File.open('packages.config'))
 ezyBuild_version = XPath.first(packages_xml, "/packages/package[@id='EzyWebwerkstaden.ezyBuild-Rake']").attributes.get_attribute("version").value
 ezyBuildDir = "./packages/EzyWebwerkstaden.ezyBuild-Rake.#{ezyBuild_version}/build"
 if (!File.exists?(ezyBuildDir))
   puts "fetching ezyBuild-Rake version: #{ezyBuild_version}"
-  # Fetch with Mono image which contains nuget. Nuget is easier to work with non-dotnet packages than dotnet restore.
-  # TODO: do GCR_PAT so that it is NOT printed to the output
-  sh "docker run --rm -e GCR_PAT=#{ENV["GCR_PAT"]} -v #{root}:/app mono:6.12.0.107 /bin/sh -c \" \
-      cd /app && \      
+  sh "docker run --rm --env-file ./.env -v #{root}:/app ghcr.io/ezywebwerkstaden/ezy.devopstools /bin/sh -c \" \
+      cd /app && \
       nuget restore ./packages.config -PackagesDirectory ./packages\""
 elsif
   puts "Skip fetching ezyBuild-Rake version: #{ezyBuild_version}, package already downloaded"
