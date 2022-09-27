8456a91b828f28bd7907d3b5fbe1e937dad20ae7
Podolak, Lukasz <lukasz.podolak.ctr@sabre.com>
2022-09-27 15:50:55 +0200

    EZYC-5478 :gear: upgrade to ezyBuild 1.0.113 so that I can remove NEXUS_ROOT_NAMESPACE hack
    * also, now we have a single place of where we define target image path in nexus

--- rake-build/ToolsGroup.rb
+++ rake-build/ToolsGroup.rb
@@ -8,11 +8,7 @@ class ToolsGroup < ToolsGroupBase
       name: 'docker-push-from-github-to-nexus',
       tools_group: self,
       source_registry: DockerRegistry.github_ezy,
-      target_registry: DockerRegistry.sabre_nexus,
-      image_map: {
-        'ezy.identityserver.sts.identity' => 'ezy-identityserver-sts-identity',
-        'ezy.identityserver.admin' => 'ezy-identityserver-admin',
-      })
+      target_registry: DockerRegistry.sabre_nexus)
 
     @tools = [docker_push_from_github_to_nexus]
   end
